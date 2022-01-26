fdd9f4df206131f5f9a5e55b838d30fed746447a
shadowsental <mattias.celius@gmail.com>
2022-01-26 15:53:40 +0100

    Added more properties to Employee.cs

--- MVCTest/Models/Employee.cs
+++ MVCTest/Models/Employee.cs
@@ -4,5 +4,9 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime BirthDate { get; set; }
     }
 }
